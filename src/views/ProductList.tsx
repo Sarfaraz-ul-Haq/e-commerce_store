80234b9176153ec63374d75157e871b4b44b8b59
Sarfaraz ul Haq <isarfaraz80@gmail.com>
2024-06-08 12:33:12 +0500

    basic product list section (carousel)

--- src/views/ProductList.tsx
+++ src/views/ProductList.tsx
@@ -1,4 +1,6 @@
 import ProductCard from "@/components/ProductCard";
+import products from "@/data/products";
+import Image from "next/image";
 
 import { Card, CardContent } from "@/components/ui/card";
 import {
@@ -21,12 +23,16 @@ function ProductList() {
         className="w-full max-w-sm"
       >
         <CarouselContent>
-          {Array.from({ length: 11 }).map((_, index) => (
+          {products.map((product, index) => (
             <CarouselItem key={index} className="md:basis-1/2 lg:basis-1/3">
               <div className="p-1">
                 <Card>
                   <CardContent className="flex aspect-square items-center justify-center p-6">
-                    <span className="text-3xl font-semibold">{index + 1}</span>
+                    <span className="text-3xl font-semibold">
+                      <Image src={product.image} alt="" />
+                      <div>{product.name}</div>
+                      <div>{product.price}</div>
+                    </span>
                   </CardContent>
                 </Card>
               </div>
