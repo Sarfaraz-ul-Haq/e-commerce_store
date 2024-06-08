7e173ce5b8e4c64f619c5b9d8025dce01e46bfb2
Sarfaraz ul Haq <isarfaraz80@gmail.com>
2024-06-08 12:31:44 +0500

    ProductCart with props

--- src/components/ProductCard.tsx
+++ src/components/ProductCard.tsx
@@ -1,5 +1,24 @@
-function ProductCard({ children }: any) {
-  return <div className="bg-slate-200 w-[280px] h-[300px]">{children}</div>;
+import Image from "next/image";
+import { StaticImageData } from "next/image";
+
+function ProductCard({
+  image,
+  name,
+  price,
+}: {
+  image: StaticImageData;
+  name: string;
+  price: string;
+}) {
+  return (
+    <div>
+      <div className="bg-slate-200 w-[280px] h-[300px]  justify-center items-center">
+        <Image src={image} alt={name} />
+      </div>
+      <div>{name}</div>
+      <div>{price}</div>
+    </div>
+  );
 }
 
 export default ProductCard;
