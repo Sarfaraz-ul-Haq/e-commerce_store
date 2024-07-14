ef884e5d813a3f061b07a9050e423ded945b2691
Sarfaraz ul Haq <isarfaraz80@gmail.com>
2024-07-14 11:37:45 +0500

    return all products

--- src/app/category/[slug]/page.tsx
+++ src/app/category/[slug]/page.tsx
@@ -2,6 +2,10 @@ import products from "@/data/products";
 import ProductCard from "@/components/ProductCard";
 
 function getProductsByCategory(category: string) {
+  if (category === "products") {
+    return products;
+  }
+
   return products.filter((product) => product.category === category);
 }
 
@@ -9,7 +13,7 @@ export default function Page({ params }: { params: { slug: any } }) {
   const products = getProductsByCategory(params.slug);
 
   return (
-    <div className="mt-14 flex gap-3 justify-between flex-wrap">
+    <div className="mt-14 mb-28 flex gap-[42px] justify-between flex-wrap">
       {products.map((product: any) => {
         return (
           <ProductCard
