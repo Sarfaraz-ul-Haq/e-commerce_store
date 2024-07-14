a0b8dee05ffc5afc893d3be4ae8dba7fce21d4a2
Sarfaraz ul Haq <isarfaraz80@gmail.com>
2024-07-14 11:34:53 +0500

    product schema

--- sanity/schema.ts
+++ sanity/schema.ts
@@ -1,5 +1,6 @@
-import { type SchemaTypeDefinition } from 'sanity'
+import { type SchemaTypeDefinition } from "sanity";
+import { product } from "./schema/product";
 
 export const schema: { types: SchemaTypeDefinition[] } = {
-  types: [],
-}
+  types: [product],
+};
---
+++ sanity/schema/product.ts
@@ -0,0 +1,29 @@
+import { defineField, defineType } from "sanity";
+
+export const products = defineType({
+  name: "product",
+  title: "Product",
+  type: "document",
+  fields: [
+    defineField({
+      name: "Title",
+      type: "string",
+    }),
+    defineField({
+      name: "Price",
+      type: "number",
+    }),
+    defineField({
+      name: "Category",
+      type: "string",
+    }),
+    defineField({
+      name: "Description",
+      type: "string",
+    }),
+    defineField({
+      name: "Image",
+      type: "image",
+    }),
+  ],
+});
