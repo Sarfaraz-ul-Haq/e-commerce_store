c9ab503b70891c626094ba83f70f167504124a07
Sarfaraz ul Haq <isarfaraz80@gmail.com>
2024-08-01 01:32:52 +0500

    add projectId + dataset: production

--- sanity/lib/client.ts
+++ sanity/lib/client.ts
@@ -3,7 +3,7 @@ import { createClient } from "next-sanity";
 import { apiVersion, projectId, useCdn } from "../env";
 
 export const client = createClient({
-  projectId,
+  projectId: "pi6nfunr",
   dataset: "production",
   apiVersion,
   useCdn: true,
