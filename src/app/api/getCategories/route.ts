bc9a9f476ba218de3de05012858db06075a40ebb
Sarfaraz ul Haq <isarfaraz80@gmail.com>
2024-07-31 23:53:05 +0500

    test route handlers

--- src/app/api/getCategories/route.ts
+++ src/app/api/getCategories/route.ts
@@ -1,27 +1,46 @@
-import { NextResponse, NextRequest } from "next/server";
+import { NextResponse } from "next/server";
 import { client } from "@/lib/sanityClient";
 
 export async function GET() {
   try {
+    console.log("fetching...");
+
     const response = await client.fetch(`
       *[_type == 'category'] {
         _id,
         title,
         "products": *[_type == 'product' && references(^._id)] {
           _id,
-          title,
+          title, 
           price,
           description,
           stock,
-          "image": image.asset->url,
-          "category": category->title
+          "images": images[]{asset->{url}},
+          "category": category->title,
+          "slug": slug.current
         }
       }
     `);
+
+    console.log("Fetched data: ", response);
+
+    if (!response || response.length === 0) {
+      return NextResponse.json(
+        { message: "No categories found" },
+        { status: 404 }
+      );
+    }
+
     return NextResponse.json(response);
   } catch (error) {
-    return NextResponse.json({
-      message: JSON.stringify(error),
-    });
+    const errorMessage =
+      error instanceof Error ? error.message : "An unknown error occurred";
+
+    return NextResponse.json(
+      {
+        message: `Failed to fetch categories: ${errorMessage}`,
+      },
+      { status: 500 }
+    );
   }
 }
