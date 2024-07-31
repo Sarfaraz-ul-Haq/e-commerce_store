383ff45be9457b79cecf09ffa92ba3861aab8828
Sarfaraz ul Haq <isarfaraz80@gmail.com>
2024-07-31 23:53:42 +0500

    edit category dynamic route

--- src/app/category/[slug]/page.tsx
+++ src/app/category/[slug]/page.tsx
@@ -1,30 +1,58 @@
-import products from "@/data/products";
-import ProductCard from "@/components/ProductCard";
+"use client";
 
-function getProductsByCategory(category: string) {
-  if (category === "products") {
-    return products;
+import useSWR from "swr";
+import ProductCard from "@/components/ProductCard";
+import Link from "next/link";
+import Product from "@/types/Product";
+import Category from "@/types/Category";
+
+const fetcher = async (url: string) => {
+  const res = await fetch(url, { cache: "no-store" });
+  if (!res.ok) {
+    throw new Error("Failed to fetch");
+  }
+  return await res.json();
+};
+
+export default function Page({ params }: { params: { slug: string } }) {
+  const { data, error, isLoading } = useSWR<Category[]>(
+    "/api/getCategories",
+    fetcher
+  );
+
+  if (error) {
+    return <div>Failed to load categories.</div>;
+  }
+  if (isLoading) return <div>Loading...</div>;
+
+  let products: Product[] = [];
+
+  if (params.slug === "products") {
+    data?.forEach((category) => {
+      products = products.concat(category.products);
+    });
+  } else {
+    const selectedCategory = data?.find(
+      (category: Category) =>
+        category.title.toLowerCase() === params.slug.toLowerCase()
+    );
+    if (selectedCategory) {
+      products = selectedCategory.products;
+    }
   }
 
-  return products.filter((product) => product.category === category);
-}
-
-export default function Page({ params }: { params: { slug: any } }) {
-  const products = getProductsByCategory(params.slug);
-
   return (
-    <div className="mt-14 mb-28 flex gap-[42px] justify-between flex-wrap">
-      {products.map((product: any) => {
-        return (
-          <ProductCard
-            key={product.id}
-            image={product.image}
-            name={product.name}
-            category={product.category}
-            price={product.price}
-          />
-        );
-      })}
+    <div className="mt-14 mb-28 flex flex-wrap justify-center gap-6">
+      {products.map((product: Product) => (
+        <ProductCard
+          key={product._id}
+          image={product.images[0]?.asset.url}
+          name={product.title}
+          category={product.category}
+          price={product.price}
+          slug={product.slug}
+        />
+      ))}
     </div>
   );
 }
