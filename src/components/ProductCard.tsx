60410adefb19fe8f1248780ef0356e03170aa1b4
Sarfaraz ul Haq <isarfaraz80@gmail.com>
2024-07-31 23:54:08 +0500

    edit product cart

--- src/components/ProductCard.tsx
+++ src/components/ProductCard.tsx
@@ -1,5 +1,4 @@
 import Image from "next/image";
-import { StaticImageData } from "next/image";
 import Link from "next/link";
 
 function ProductCard({
@@ -7,28 +6,33 @@ function ProductCard({
   name,
   price,
   category,
+  slug,
 }: {
-  image: StaticImageData;
+  image: string;
   name: string;
-  price: string;
+  price: number;
   category?: string;
+  slug: string;
 }) {
   return (
     <div className="mb-5">
-      <Link href={"/"}>
+      <Link href={`/product/${slug}`}>
         <div
           className={
-            "bg-gray-200 w-[260px] h-[265px] justify-center items-center mb-5"
+            "bg-gray-200 w-[260px] h-[265px] justify-center items-center mb-5 relative"
           }
         >
-          <Image src={image} alt={name} />
+          <Image src={image} alt={name} layout="fill" objectFit="cover" />
         </div>
         <div className="font-bold mb-1">{name}</div>
         {category && (
           <div className="font-semibold text-gray-400 mb-2">{category}</div>
         )}
-        <div className="font-bold text-xl">{price}</div>
+        <div className="font-bold text-xl">${price}</div>
       </Link>
+      <button className="mt-2 bg-gray-950 text-white py-1 px-4 rounded">
+        Add to Cart
+      </button>
     </div>
   );
 }
